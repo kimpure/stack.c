b052e772e9af7d940c27299a991e63d6c1caa0a8
kimpure <gimleni593@gmail.com>
2025-04-18 14:24:18 +0000

    first commit

---
+++ stack.c
@@ -0,0 +1,38 @@
+// c 에서 원래는 class 만들어서 쓰고싶었지만
+// 억지로 만들어서 쓸수는 있지만 그냥 뭔가 보기안좋아서.. 기각
+
+#include <stdio.h>
+
+typedef struct {
+    int *stack; // stack->stack (포인터 위치)
+    int rsp; // stack->rsp (stack 현제위치)
+} Stack;
+
+int push(Stack* stack, int value) {
+    stack->stack[stack->rsp] = value;
+    stack->rsp = stack->rsp + 1;
+}
+
+int pop(Stack* stack) {
+    // 파라미터로 stack 의 포인터위치 받음
+
+    if (stack->rsp == 0) {
+        return 0;
+    }
+
+    stack->rsp = stack->rsp - 1;
+    int value = stack->stack[stack->rsp];
+    stack->stack[stack->rsp] = 0;
+
+    return value;
+}
+
+int main() {
+    Stack *stack;
+    stack->rsp = 0;
+    stack->stack[5];
+
+    push(stack, 5);
+
+    printf("%d\n", stack->stack[0]);
+}
